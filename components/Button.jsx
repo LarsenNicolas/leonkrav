8cc0499fac0283bf20db87feb99f24af08445137
Nicolas Larsen <nicolas.larsen96@gmail.com>
2025-04-29 13:02:50 -0300

    Fix button tsx

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,10 +1,22 @@
-export default function Button({ children, onClick, className }) {
+'use client';
+
+import { ReactNode, FC, MouseEvent } from 'react';
+
+interface ButtonProps {
+    children: ReactNode;
+    onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+    className?: string;
+}
+
+const Button: FC<ButtonProps> = ({ children, onClick, className }) => {
     return (
         <button
             onClick={onClick}
-            className={`bg-black cursor-pointer w-1/3 text-white px-4 py-4 my-8 rounded hover:bg-[#a5732db5]-700 transition ${className}`}
+            className={`bg-black text-white px-4 py-4 my-8 rounded cursor-pointer w-1/3 hover:bg-opacity-80 transition-all ${className || ''}`}
         >
             {children}
         </button>
     );
-}
+};
+
+export default Button;
